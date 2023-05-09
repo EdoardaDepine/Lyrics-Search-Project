77a38843792005465b94399b02962f525acd774c
EdoardaDepine <edoarda.depine@gmail.com>
2023-05-09 11:35:42 -0300

    script responsável por buscar música em api externa, por nome do artista e nome da música; e também, por salvar a  música no local storage

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
+const getSavedMusics = () => {
+    return JSON.parse(localStorage.getItem("music")) || [];
+};
+
+const setSavedMusics = (music) => {
+    if (localStorage.hasOwnProperty("music")) {
+        localStorage.setItem("music", JSON.stringify([...getSavedMusics(), music]));
+    } else {
+        localStorage.setItem("music", JSON.stringify([music]));
+    }
+}
+
 function findLyrics(artist, song) {
-  return fetch(
-    `https://api.vagalume.com.br/search.php?&art=${artist}y&mus=${song}`
-  );
+    return fetch(
+        `https://api.vagalume.com.br/search.php?&art=${artist}&mus=${song}`
+    );
 }
 
 const formulary = document.querySelector("#form");
 formulary.addEventListener("submit", (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  try {
-    doSubmit();
-  } catch (error) {
-    const errorArtist = document.querySelector("#errorArtist");
-    errorArtist.innerHTML = error.message;
-  }
+    try {
+        doSubmit();
+    } catch (error) {
+        const errorArtist = document.querySelector("#errorArtist");
+        errorArtist.innerHTML = error.message;
+    }
 });
 
 async function doSubmit() {
-  const artist = document.querySelector("#artistInput");
-  const lyric = document.querySelector("#lyric");
-  const song = document.querySelector("#songInput");
+    const artist = document.querySelector("#artistInput");
+    const lyric = document.querySelector("#lyric");
+    const song = document.querySelector("#songInput");
 
-  if (artist.value.length === 0 || song.value.length === 0) {
-    throw new Error("Please select a artist and a song");
-  } else {
-    lyric.innerHTML = "Loading...";
+    if (artist.value.length === 0 || song.value.length === 0) {
+        throw new Error("Please select a artist and a song");
+    } else {
+        lyric.innerHTML = "Loading...";
 
-    const lyricResponse = await findLyrics(artist.value, song.value);
-    const musicObject = await lyricResponse.json();
-    const musicText = musicObject.mus[0].text;
-    lyric.innerHTML = musicText;
-  }
+        const lyricResponse = await findLyrics(artist.value, song.value);
+        const musicObject = await lyricResponse.json();
+        const musicText = musicObject.mus[0].text;
+
+        if (!getSavedMusics().some((music) => music === musicText)) {
+            setSavedMusics(musicText)
+        }
+
+        lyric.innerHTML = musicText;
+    }
 }
